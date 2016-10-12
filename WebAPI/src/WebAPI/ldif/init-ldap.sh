4a963667319e3857aed4f1aac6f3ada90a5e5139
Grégory Compte <chibiketan@gmail.com>
2016-10-12 23:02:17 +0200

    ça commence à ressembler à quelquechose. Il ne manque plus au conteneur principal d'attendre que celui de ldap réponde avant de lancer l'initialisation

--- WebAPI/src/WebAPI/ldif/init-ldap.sh
+++ WebAPI/src/WebAPI/ldif/init-ldap.sh
@@ -1,4 +1,6 @@
-﻿status () {
+#!/bin/bash
+
+status () {
   echo "---> ${@}" >&2
 }
 
@@ -6,11 +8,11 @@ set -x
 : LDAP_ROOTPASS=${LDAP_ROOTPASS}
 : LDAP_FQN_DOMAIN=${LDAP_FQN_DOMAIN}
 
-if [ ! -e /shared/ketan_ldap_bootstrapped ]; then
-  status "Creation des elements pour le premier lanncement"
-
-  ldapadd -v -h ketan.ldap:389 -c -x -D cn=admin,$LDAP_FQN_DOMAIN -w $LDAP_ROOTPASS -f /mnt/ldif/user.ldif
-  touch /shared/ketan_ldap_bootstrapped
+if [ ! -e /mnt/shared/ketan_ldap_bootstrapped ]; then
+  status "Creation des elements pour le premier lancement"
+  ping ketan.ldap
+  ldapadd -v -h ketan.ldap:389 -c -x -D cn=admin,$LDAP_FQN_DOMAIN -w $LDAP_ROOTPASS -f /mnt/ldif/user.ldif \
+  && touch /mnt/shared/ketan_ldap_bootstrapped
 else
   status "Elements deja crees"
 fi
