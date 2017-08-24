43206694a1f69d0a375dc8c46344674381dafbcb
Grégory COMPTE <chibiketan@gmail.com>
2017-08-24 20:48:45 +0200

    Suppression des fichiers générés à chaque build

--- WebAPI/src/WebAPI/wwwroot/appScripts/viewmodel/authenticate-response.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var AuthenticateResponse = (function () {
-    function AuthenticateResponse() {
-    }
-    return AuthenticateResponse;
-}());
-exports.AuthenticateResponse = AuthenticateResponse;
-//# sourceMappingURL=authenticate-response.js.map
