199c521c38170211385eae42c5a7c21439d656cb
Grégory COMPTE <chibiketan@gmail.com>
2017-10-05 23:23:49 +0200

    Début d'un projet de service d'auth avec une base mongodb (sans tests pour l'instant)

---
+++ service.authentication/alimentation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace alimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionSettings = new MongoDB.Driver.MongoClientSettings();
+
+            connectionSettings.Server = new MongoServerAddress("localhost");
+            var connection = new MongoDB.Driver.MongoClient(connectionSettings);
+            var db = connection.GetDatabase("test");
+            var collection = db.GetCollection<MongoDB.Bson.BsonArray>("Roles");
+            var search = collection.FindSync(new JsonFilterDefinition<BsonArray>(""));
+
+            search.MoveNext();
+            foreach (var el in search.Current)
+            {
+                Console.WriteLine(el.ToString());
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
