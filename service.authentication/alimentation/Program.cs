0dedb2c429e9bfef1fe805f45c6d2d8406e3b646
Grégory COMPTE <chibiketan@gmail.com>
2017-10-06 21:02:51 +0200

    - Ajout de la création de la collection des rôles et des rôles par défaut s'ils n'existent pas

--- service.authentication/alimentation/Program.cs
+++ service.authentication/alimentation/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using MongoDB.Bson;
@@ -10,6 +12,7 @@ namespace alimentation
     class Program
     {
         private static IConfigurationRoot s_configuration;
+        private const string CREATION_USER = "System";
 
         private static IMongoDatabase GetMongoDatabase()
         {
@@ -35,8 +38,113 @@ namespace alimentation
             return connection.GetDatabase(databaseName);
         }
 
+        private static async Task<bool> CheckIfCollectionExist(IMongoDatabase database, string collectionName)
+        {
+            var option = new ListCollectionsOptions()
+            {
+                Filter = new BsonDocument("name", collectionName)
+            };
+
+            var collection = await database.ListCollectionsAsync(option);
+            
+            return await collection.AnyAsync();
+        }
+
+        private static void WriteCreationStatus(CreationStatus status)
+        {
+            var oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("[");
+            switch (status)
+            {
+                case CreationStatus.Ok:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("OK");
+                    break;
+                case CreationStatus.Ko:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("KO");
+                    break;
+                case CreationStatus.None:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write("--");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(status), status, "Invalid value");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("]");
+            Console.ForegroundColor = oldColor;
+        }
+
+        private static void WriteCreationMessage(string message, int maxLength = 80)
+        {
+            message = message ?? "";
+            if (message.Length > maxLength)
+            {
+                message = message.Substring(0, maxLength);
+            }
+
+            Console.Write(message);
+            Console.Write(new string(' ', maxLength - message.Length));
+        }
+
+        private static async Task CreateRoleCollection(IMongoDatabase db)
+        {
+            const string collectionName = "Role";
+            try
+            {
+                WriteCreationMessage("Creating \"Role\" collection.");
+                // On vérifie si la collection existe déjà
+                if (await CheckIfCollectionExist(db, collectionName))
+                {
+                    WriteCreationStatus(CreationStatus.None);
+                    return;
+                }
+
+                // on initialise la collection
+                await db.CreateCollectionAsync(collectionName);
+                var collection = db.GetCollection<Role>(collectionName);
+                var roles = new[]
+                {
+                    new Role
+                    {
+                        Name = "Admin",
+                        CreationDate = DateTime.Now,
+                        CreationUser = CREATION_USER
+                    },
+                    new Role
+                    {
+                        Name = "Seller",
+                        CreationDate = DateTime.Now,
+                        CreationUser = CREATION_USER
+                    },
+                    new Role
+                    {
+                        Name = "Customer",
+                        CreationDate = DateTime.Now,
+                        CreationUser = CREATION_USER
+                    }
+                };
+                await collection.InsertManyAsync(roles);
+                WriteCreationStatus(CreationStatus.Ok);
+            }
+            catch (Exception e)
+            {
+                WriteCreationStatus(CreationStatus.Ko);
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
+            // set console output to utf8
+            Console.OutputEncoding = Encoding.UTF8;
             // Construction de la configuration, on utilise l'emplacement du binaire pour trouver le fichier de config
             s_configuration = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
@@ -46,6 +154,9 @@ namespace alimentation
 
             // création du client mongodb
             var db = GetMongoDatabase();
+
+            // création des rôles
+            CreateRoleCollection(db).Wait();
 
             var collection = db.GetCollection<MongoDB.Bson.BsonDocument>("Roles");
             var search = collection.Find(new BsonDocument()).ToCursorAsync().Result;
@@ -59,5 +170,12 @@ namespace alimentation
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
+    }
+
+    enum CreationStatus
+    {
+        Ok,
+        Ko,
+        None
     }
 }
---
+++ service.authentication/alimentation/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alimentation
+{
+    internal class Role
+    {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreationUser { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public string ModificationUser { get; set; }
+
+    }
+}
