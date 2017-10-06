53e595e0440c26b540ca10f2f43e5c69e69f8224
Grégory COMPTE <chibiketan@gmail.com>
2017-10-06 20:02:49 +0200

    - Ajout d'un fichier de configuration et les packages nécessaire pour gérer la configuration par le fichier et pas l'environnement

--- service.authentication/alimentation/Program.cs
+++ service.authentication/alimentation/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -6,13 +9,44 @@ namespace alimentation
 {
     class Program
     {
+        private static IConfigurationRoot s_configuration;
+
+        private static IMongoDatabase GetMongoDatabase()
+        {
+            var databaseName = s_configuration["db:name"];
+            var connectionSettings = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress(s_configuration["db:host"]),
+                ApplicationName = typeof(Program).FullName
+            };
+
+            // Si les identifiants sont fournis, on les utilise
+            var user = s_configuration["db:user"];
+            var pass = s_configuration["db:pass"];
+
+            if (!string.IsNullOrEmpty(user))
+            {
+                connectionSettings.Credentials = new []{ MongoCredential.CreateCredential(databaseName, user, pass) };
+            }
+
+            // Création de la connexion
+            var connection = new MongoClient(connectionSettings);
+
+            return connection.GetDatabase(databaseName);
+        }
+
         static void Main(string[] args)
         {
-            var connectionSettings = new MongoDB.Driver.MongoClientSettings();
+            // Construction de la configuration, on utilise l'emplacement du binaire pour trouver le fichier de config
+            s_configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables()
+                .Build();
 
-            connectionSettings.Server = new MongoServerAddress("localhost");
-            var connection = new MongoDB.Driver.MongoClient(connectionSettings);
-            var db = connection.GetDatabase("test");
+            // création du client mongodb
+            var db = GetMongoDatabase();
+
             var collection = db.GetCollection<MongoDB.Bson.BsonDocument>("Roles");
             var search = collection.Find(new BsonDocument()).ToCursorAsync().Result;
 
