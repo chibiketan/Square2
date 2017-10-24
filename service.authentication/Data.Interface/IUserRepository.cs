7ca9769be2c01c38e2a1d3ded10c5cd547296a35
Grégory COMPTE <chibiketan@gmail.com>
2017-10-24 20:33:49 +0200

    - Ajout des projets pour la gestion de l'accès aux données

---
+++ service.authentication/Data.Interface/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ketan.Square2.Service.Authentication.Data.Interface
+{
+    public interface IUserRepository
+    {
+    }
+}
---
+++ service.authentication/Data/UserMongoRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Ketan.Square2.Service.Authentication.Data.Interface;
+
+namespace Ketan.Square2.Service.Authentication.Data
+{
+    public class UserMongoRepository : IUserRepository
+    {
+    }
+}
