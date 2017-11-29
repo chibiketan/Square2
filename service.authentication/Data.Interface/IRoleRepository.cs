151a638bb35abde78ee2654ee51b191d23303981
Grégory COMPTE <chibiketan@gmail.com>
2017-11-29 21:05:01 +0100

    - Précision des exceptions attendues pour les méthode de l'interface IRoleRepository

--- service.authentication/Data.Interface/IRoleRepository.cs
+++ service.authentication/Data.Interface/IRoleRepository.cs
@@ -12,6 +12,7 @@ namespace Ketan.Square2.Service.Authentication.Data.Interface
         /// </summary>
         /// <param name="role">Le role à créer, ne doit pas être null</param>
         /// <exception cref="ArgumentNullException">Exception lancée si le paramètre <paramref name="role"/> est null</exception>
+        /// <exception cref="ObjectExistsException">Exception lancée si le paramètre <paramref name="role"/> contient un nom qui existe déjà dans le repository</exception>
         Task CreateRoleAsync(Role role);
 
         /// <summary>
@@ -25,6 +26,8 @@ namespace Ketan.Square2.Service.Authentication.Data.Interface
         /// </summary>
         /// <param name="name">Nom du rôle à récupérer</param>
         /// <returns>Le rôle s'il a été trouvé, null sinon</returns>
+        /// <exception cref="ArgumentNullException">Exception lancée si le paramètre <paramref name="name"/> est null</exception>
+        /// <exception cref="ArgumentException">Exception lancée si le paramètre <paramref name="name"/> est vide</exception>
         Task<Role> GetRoleByNameAsync(string name);
 
     }
