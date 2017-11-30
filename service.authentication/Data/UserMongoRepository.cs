c39da5eca421a4626dd238bb46b8c499fd4124e5
Grégory COMPTE <chibiketan@gmail.com>
2017-11-30 19:44:30 +0100

    - Ecriture du code pour faire passer les tests de création d'un utilisateur.

--- service.authentication/Data/UserMongoRepository.cs
+++ service.authentication/Data/UserMongoRepository.cs
@@ -3,20 +3,54 @@ using System.Threading.Tasks;
 using Ketan.Square2.Service.Authentication.Data.Interface;
 using Ketan.Square2.Service.Authentication.Model;
 using Ketan.Square2.Service.Authentication.Model.Configuration;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace Ketan.Square2.Service.Authentication.Data
 {
-    public class UserMongoRepository : IUserRepository
+    public class UserMongoRepository : BaseMongoRepository<User>, IUserRepository
     {
-        private readonly DatabaseConfiguration m_configuration;
-
         public UserMongoRepository(DatabaseConfiguration configuration)
+            : base(configuration, "User")
         {
-            m_configuration = configuration;
         }
-        public Task CreateUserAsync(User user)
+        public async Task CreateUserAsync(User user)
         {
-            throw new NotImplementedException();
+            if (null == user)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            // On test si l'utilisateur existe déjà
+            var existingUSer = await m_collection.AsQueryable().FirstOrDefaultAsync(r => r._id == user._id);
+
+            if (null != existingUSer)
+            {
+                throw new ObjectExistsException($"An User with id \"{user._id}\" already exists");
+            }
+
+            // On test si l'utilisateur existe déjà
+            existingUSer = await m_collection.AsQueryable().FirstOrDefaultAsync(r => r.Login == user.Login);
+
+            if (null != existingUSer)
+            {
+                throw new ObjectExistsException($"An User with login \"{user.Login}\" already exists");
+            }
+
+            StripDates(user);
+            await m_collection.InsertOneAsync(user);
+        }
+
+        protected override void OnCollectionCreated(IMongoCollection<User> collection)
+        {
+            // Création d'un index sur le champ Login
+            collection.Indexes.CreateOneAsync(Builders<User>.IndexKeys.Ascending(u => u.Login), new CreateIndexOptions { Name = "User_Login", Unique = true }).Wait();
+        }
+
+        private void StripDates(User user)
+        {
+            user.CreationDate = user.CreationDate.StripTick();
+            user.ModificationDate = user.ModificationDate.StripTick();
         }
     }
 }
